25a541bb8ecb5c56496523f2742615d64a422097
Roman Golovkov <romangolovkov@mail.ru>
2024-09-09 22:06:40 +0800

    lesson 13 solution corrected

--- lesson_13/main.py
+++ lesson_13/main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route("/logo/<name>/<surname>/<int:age>")
 def index(name, surname, age):
     if age % 10 == 1 and age != 11:
-        return render_template("base.html", name=name, surname=surname, age=age, year='год')
+        return render_template("base.html", name=name, surname=surname, age=age, year='год', title=f'{name} {surname}')
     elif age % 10 in (2, 3, 4) and age not in (12, 13, 14):
-        return render_template("base.html", name=name, surname=surname, age=age, year='года')
+        return render_template("base.html", name=name, surname=surname, age=age, year='года', title=f'{name} {surname}')
     else:
-        return render_template("base.html", name=name, surname=surname, age=age, year='лет')
+        return render_template("base.html", name=name, surname=surname, age=age, year='лет', title=f'{name} {surname}')
 
 
 
